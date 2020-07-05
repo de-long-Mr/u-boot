d1b7bf60023d45b3045cdc06e475f69b4f38b4c2
de-long-Mr <delongli@aliyun.com>
2020-07-05 22:26:30 +0800

    Modify the TF card part of the device tree

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,5 +1,5 @@
 #define U_BOOT_DATE "Jul 05 2020"
-#define U_BOOT_TIME "17:09:29"
+#define U_BOOT_TIME "22:23:26"
 #define U_BOOT_TZ "+0800"
 #define U_BOOT_DMI_DATE "07/05/2020"
 #define U_BOOT_BUILD_DATE 0x20200705
